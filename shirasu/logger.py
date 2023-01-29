1bb02fe5c3a93fbf18ff4ede5963bf36602e6875
kifuan <kifuan@foxmail.com>
2023-01-29 23:33:46 +0800

    chore: update logger format

--- shirasu/logger.py
+++ shirasu/logger.py
@@ -5,7 +5,7 @@ from loguru import logger as logger
 logger.configure(
     handlers=[{
         'sink': sys.stdout,
-        'format': '<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | '
+        'format': '<green>{time:YY-MM-DD HH:mm:ss}</green> | '
                   '<level>{level: <8}</level> | '
                   '<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - '
                   '<level>{message}</level>'
