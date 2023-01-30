0610c39676a1de8314f9c1c8666b27167acbb937
kifuan <kifuan@foxmail.com>
2023-01-30 13:14:49 +0800

    refactor: inject by name rather than by type

--- shirasu/di.py
+++ shirasu/di.py
@@ -6,8 +6,8 @@ _T = TypeVar('_T')
 
 
 class DependencyError(Exception):
-    def __init__(self, deps: list[type], prompt: str) -> None:
-        super().__init__(f'{prompt}: {", ".join(d.__name__ for d in deps)}')
+    def __init__(self, deps: list[str], prompt: str) -> None:
+        super().__init__(f'{prompt}: {", ".join(deps)}')
         self.deps = deps
 
 
@@ -16,7 +16,7 @@ class UnknownDependencyError(DependencyError):
     Dependency not found.
     """
 
-    def __init__(self, deps: list[type]) -> None:
+    def __init__(self, deps: list[str]) -> None:
         super().__init__(deps, 'unknown dependencies')
 
 
@@ -25,12 +25,12 @@ class CircularDependencyError(DependencyError):
     Circular dependency.
     """
 
-    def __init__(self, deps: list[type]) -> None:
+    def __init__(self, deps: list[str]) -> None:
         super().__init__(deps, 'circular dependencies')
 
 
 class DuplicateDependencyProviderError(DependencyError):
-    def __init__(self, dep: type) -> None:
+    def __init__(self, dep: str) -> None:
         super().__init__([dep], 'duplicate dependency provider')
 
 
@@ -41,26 +41,23 @@ class DependencyInjector:
     """
 
     def __init__(self) -> None:
-        self._providers: dict[type, Callable[..., Awaitable[Any]]] = {}
+        self._providers: dict[str, Callable[..., Awaitable[Any]]] = {}
 
-    async def _inject_func_args(self, func: Callable[..., Awaitable[Any]], *inject_for: type) -> dict[str, Any]:
-        params = {
-            name: param.annotation
-            for name, param in inspect.signature(func).parameters.items()
-        }
+    async def _inject_func_args(self, func: Callable[..., Awaitable[Any]], *inject_for: str) -> dict[str, Any]:
+        deps = [name for name in inspect.signature(func).parameters]
 
-        if unknown_deps := [typ for typ in params.values() if typ not in self._providers]:
+        if unknown_deps := [dep for dep in deps if dep not in self._providers]:
             raise UnknownDependencyError(unknown_deps)
 
-        if circular_deps := [typ for typ in params.values() if typ in inject_for]:
+        if circular_deps := [dep for dep in deps if dep in inject_for]:
             raise CircularDependencyError(circular_deps)
 
         return {
-            name: await self._apply(self._providers[typ], typ, *inject_for)
-            for name, typ in params.items()
+            dep: await self._apply(self._providers[dep], dep, *inject_for)
+            for dep in deps
         }
 
-    async def _apply(self, func: Callable[..., Awaitable[_T]], *apply_for: type) -> _T:
+    async def _apply(self, func: Callable[..., Awaitable[_T]], *apply_for: str) -> _T:
         injected_args = await self._inject_func_args(func, *apply_for)
         return await func(**injected_args)
 
@@ -71,16 +68,13 @@ class DependencyInjector:
             return await self._apply(func)
         return wrapper
 
-    def provide(self, func: Callable[..., Awaitable[_T]], *, check_duplicate: bool = True) -> None:
+    def provide(self, name: str, func: Callable[..., Awaitable[_T]], *, check_duplicate: bool = True) -> None:
         assert inspect.iscoroutinefunction(func), 'Dependency provider must be async.'
 
-        typ = inspect.signature(func).return_annotation
-        assert typ is not None and typ != inspect.Parameter.empty, 'Dependency provider must have return type.'
+        if check_duplicate and name in self._providers:
+            raise DuplicateDependencyProviderError(name)
 
-        if check_duplicate and typ in self._providers:
-            raise DuplicateDependencyProviderError(typ)
-
-        self._providers[typ] = func
+        self._providers[name] = func
 
 
 di = DependencyInjector()
@@ -100,13 +94,13 @@ def inject():
     return deco
 
 
-def provide(*, check_duplicate: bool = True) -> Callable[[Callable[..., Awaitable[_T]]], Callable[..., Awaitable[_T]]]:
+def provide(name: str, *, check_duplicate: bool = True) -> Callable[[Callable[..., Awaitable[_T]]], Callable[..., Awaitable[_T]]]:
     """
     Registers provider using decorator.
     :return: the decorator to register provider.
     """
 
     def deco(func: Callable[..., Awaitable[_T]]) -> Callable[..., Awaitable[_T]]:
-        di.provide(func, check_duplicate=check_duplicate)
+        di.provide(name, func, check_duplicate=check_duplicate)
         return func
     return deco
--- test/test_di.py
+++ test/test_di.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Any
 from shirasu.di import (
     DependencyInjector,
     CircularDependencyError,
@@ -7,43 +8,47 @@ from shirasu.di import (
 )
 
 
-FLOAT = 12.
-INT = 120
+NAME = 'Taffy'
+YEAR = 1883
 
 
-async def provide_int(_: float) -> int:
-    return INT
+async def provide_name(year: int) -> str:
+    assert year == YEAR
+    return NAME
 
 
-async def provide_float_circular(_: int) -> float:
-    return FLOAT
+async def provide_year() -> int:
+    return YEAR
 
 
-async def provide_float_unknown_dep(_: str) -> float:
-    return FLOAT
+async def provide_year_circular(name: str) -> int:
+    pytest.fail(f'Failed because name {name} is circular.')
+    return YEAR
 
 
-async def provide_float() -> float:
-    return FLOAT
+async def provide_year_unknown(unknown_var: Any) -> int:
+    pytest.fail(f'Failed because unknown var {unknown_var} should not be injected')
+    return YEAR
 
 
-async def user(i: int, f: float) -> None:
-    assert i == INT and f == FLOAT
+async def user(year: int, name: str) -> None:
+    assert year == YEAR
+    assert name == NAME
 
 
 @pytest.mark.asyncio
 async def test_di():
     di = DependencyInjector()
-    di.provide(provide_int)
-    di.provide(provide_float)
+    di.provide('name', provide_name)
+    di.provide('year', provide_year)
     await di.inject(user)()
 
 
 @pytest.mark.asyncio
 async def test_circular():
     di = DependencyInjector()
-    di.provide(provide_int)
-    di.provide(provide_float_circular)
+    di.provide('name', provide_name)
+    di.provide('year', provide_year_circular)
     with pytest.raises(CircularDependencyError):
         await di.inject(user)()
 
@@ -51,15 +56,15 @@ async def test_circular():
 @pytest.mark.asyncio
 async def test_unknown():
     di = DependencyInjector()
-    di.provide(provide_int)
-    di.provide(provide_float_unknown_dep)
+    di.provide('name', provide_name)
+    di.provide('year', provide_year_unknown)
     with pytest.raises(UnknownDependencyError):
         await di.inject(user)()
 
 
 def test_duplicate_provider():
     di = DependencyInjector()
-    di.provide(provide_int)
-    di.provide(provide_int, check_duplicate=False)
+    di.provide('name', provide_name)
+    di.provide('name', provide_name, check_duplicate=False)
     with pytest.raises(DuplicateDependencyProviderError):
-        di.provide(provide_int)
+        di.provide('name', provide_name)
