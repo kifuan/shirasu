0125304dc57b4f670734402809bc3a6b72c2074a
kifuan <kifuan@foxmail.com>
2023-02-01 15:04:20 +0800

    feat: add builtin addon `help`

---
+++ shirasu/addons/help.py
@@ -0,0 +1,34 @@
+from shirasu import Client, Addon, AddonPool, MessageEvent, command
+
+
+echo = Addon(
+    name='help',
+    usage='/help addon_name',
+    description='Prints usage description for certain addon.',
+)
+
+
+FORMAT = '''
+Name: {name}
+Usage: {usage}
+Description: {description}
+'''.strip()
+
+
+@echo.receive(command('help'))
+async def receive_echo(client: Client, pool: AddonPool, event: MessageEvent) -> None:
+    name = event.arg
+    if not name:
+        addons = ', '.join(a.name for a in pool)
+        await client.send(f'Available addons: {addons}')
+        return
+
+    if addon := pool.get_addon(name):
+        await client.send(FORMAT.format(
+            name=addon.name,
+            usage=addon.usage,
+            description=addon.description,
+        ))
+        return
+
+    await client.send(f'Addon {name} is not found.')
