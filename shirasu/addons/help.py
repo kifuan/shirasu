8f88f3f85e1a674c31e701de237944bfa3faae6e
kifuan <kifuan@foxmail.com>
2023-02-01 16:29:41 +0800

    feat: configurable addon `help`

--- shirasu/addons/help.py
+++ shirasu/addons/help.py
@@ -1,10 +1,16 @@
+from pydantic import BaseModel
 from shirasu import Client, Addon, AddonPool, MessageEvent, command
+
+
+class HelpConfig(BaseModel):
+    show_addon_list: bool = True
 
 
 help_addon = Addon(
     name='help',
     usage='/help addon_name',
     description='Prints usage description for certain addon.',
+    config_model=HelpConfig,
 )
 
 
@@ -16,12 +22,16 @@ Description: {description}
 
 
 @help_addon.receive(command('help'))
-async def handle_help(client: Client, pool: AddonPool, event: MessageEvent) -> None:
+async def handle_help(client: Client, config: HelpConfig, pool: AddonPool, event: MessageEvent) -> None:
     name = event.arg
     if not name:
-        addons = ', '.join(a.name for a in pool)
-        await client.send(f'Available addons: {addons}')
-        return
+        if not config.show_addon_list:
+            await client.send('The configuration to show the addon list is turned off.')
+            return
+        else:
+            addons = ', '.join(a.name for a in pool)
+            await client.send(f'Available addons: {addons}')
+            return
 
     if addon := pool.get_addon(name):
         await client.send(FORMAT.format(
